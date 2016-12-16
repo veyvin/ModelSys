b6f066ea6ec0dc67b28c04cb6e83562729b8b4a1
胡正飞 <veyvin@outlook.com>
2016-12-16 17:33:55 +0800

    添加项目文件。

---
+++ ModelSys/Launcher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelSys
+{
+    class Launcher
+    {
+
+        public static void Launch()
+        {
+            Process.Start(Path.GetFullPath("LIB/LIB2.exe"),"fbx assetbundle");
+            if (MainWindow.close)
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ ModelSys/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModelSys
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Fields
+
+        public static bool close;
+        public static MainWindow mainWindow;
+
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            myweb.Source = new Uri(Environment.CurrentDirectory + "/lib.html");
+            if (mainWindow == null)
+            {
+                mainWindow = this;
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Launcher.Launch();
+        }
+
+        private void checkBox_Close_Checked(object sender, RoutedEventArgs e)
+        {
+            close = true;
+        }
+
+        private void checkBox_Close_UnChecked(object sender, RoutedEventArgs e)
+        {
+            close = false;
+        }
+    }
+}
